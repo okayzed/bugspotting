3b37068bc014c1b1bd79aaef936f2531c0226ed6
cliffpham <cliffpham@gmail.com>
2019-06-18 12:44:16 -0700

    [revision] problem for identifying index out of bounds issue

--- src/index_outofbounds.py
+++ src/index_outofbounds.py
@@ -1,18 +1,22 @@
-# Given an unsorted array of integers, find the length of longest continuous increasing subsequence (subarray).
+# Given an unsorted array of integers, find the length of longest continuous
+# increasing subsequence (subarray).
 # Input: [1,3,5,4,7]
 # Output: 3
 
 
 def lcis(arr):
     i = 0
-    cur_count = 0
+    cur_count = 1
     max_count = 0
 
     while (i < len(arr)):
         if arr[i] < arr[i+1]:
             cur_count += 1
             max_count = max(cur_count, max_count)
-        i += 1
+            i += 1
+        else:
+            cur_count = 1
+            i += 1
 
     return max_count
 
