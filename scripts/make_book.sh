1376a01d7692f3ea6d6cfe1b6cf2cf3a2f570dc5
okay <okay@chalk>
2019-06-18 12:41:36 -0700

    fmt long lines when making book, remove margins

--- scripts/make_book.sh
+++ scripts/make_book.sh
@@ -12,18 +12,18 @@ for f in src/*.py; do
     echo "--------" >> ${MD_FILE}
 
     echo "\`\`\`python" >> ${MD_FILE}
-    cat ${f} >> ${MD_FILE}
+    cat -n ${f} | fmt -s -w 90 >> ${MD_FILE}
     echo "\`\`\`" >> ${MD_FILE}
 
     echo "Problem ${N} Output" >> ${MD_FILE}
     echo "--------" >> ${MD_FILE}
     echo "\`\`\`bash" >> ${MD_FILE}
-    python ${f} 2>&1 | sed "s/${f/\//\\\/}/Problem ${N}/g"  >> ${MD_FILE}
+    python ${f} 2>&1 | sed "s/${f/\//\\\/}/Problem ${N}/g"  | cat -n | fmt -s >> ${MD_FILE}
     echo "\`\`\`" >> ${MD_FILE}
 
     ((N++))
   fi
 
 done
-pandoc ${MD_FILE} -o ${PDF_FILE}
+pandoc ${MD_FILE} -o ${PDF_FILE} -V geometry:margin=0.5in
 evince ${PDF_FILE}
